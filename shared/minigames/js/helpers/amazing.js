3550556ae4b6756e405ca26d77d15f217e0a718b
MartinPlascencia <martin.plascencia@live.com.mx>
2017-02-14 11:11:03 -0600

    cambios index y amazing

--- shared/minigames/js/helpers/amazing.js
+++ shared/minigames/js/helpers/amazing.js
@@ -35,21 +35,21 @@ amazing.share = function(score, game){
 amazing.getGames = function(){
 	var games = [
         
-        {name:'Amazing Bros',iconName:'bros',url:'http://amazingapp.co/juegos/amazingbros/',coupon: true,mixName:'amazingbros'},
-        {name:'Zombie\n Crush',iconName:'zombie',url:'http://amazingapp.co/juegos/zombiecrush/',coupon : false,mixName:'zombiecrush'},
-        {name:'Cirquit',iconName:'cirquit',url:'http://amazingapp.co/juegos/cirquit/',coupon : false,mixName:'cirquit'},
-        {name:'Clown Rush',iconName:'clown',url:'http://amazingapp.co/juegos/clownrush/',coupon : false,mixName:'clownrush'},
-        {name:'Gravity\n Switch',iconName:'gravity',url:'http://amazingapp.co/juegos/graviswitch/',coupon : false,mixName:'graviswitch'},
-        {name:'Hexhop',iconName:'hexhop',url:'http://amazingapp.co/juegos/hexhop/',coupon : false,mixName:'hexhop'},
-        {name:'Memorama\n del Sabor',iconName:'memorama',url:'http://amazingapp.co/juegos/memorama/',coupon : false,mixName:'memorama'},
-        {name:'Jump Ward',iconName:'jump',url:'http://amazingapp.co/juegos/jumpward/',coupon : false,mixName:'jumpward'},
-        {name:'Jungle Fury',iconName:'jungle',url:'http://amazingapp.co/juegos/junglefury/',coupon : false,mixName:'junglefury'},
-        {name:'Lluvia de\n Snacks',iconName:'lluvia',url:'http://amazingapp.co/juegos/chilimbalam/',coupon : false,mixName:'chilimbalam'},
-        {name:'Store Panic',iconName:'panic',url:'http://amazingapp.co/juegos/storepanic/',coupon : false,mixName:'storepanic'},
-        {name:'Tapatopo',iconName:'tapa',url:'http://amazingapp.co/juegos/tapatopo/',coupon : false,mixName:'tapatopo'},
-        {name:'Twin Dots',iconName:'twin',url:'http://amazingapp.co/juegos/twindots/',coupon : false,mixName:'twindots'},
-        {name:'Neon Edge',iconName:'neon',url:'http://amazingapp.co/juegos/neonedge/',coupon : false,mixName:'neonedge'},
-        {name:'Cube Jump',iconName:'cube',url:'http://amazingapp.co/juegos/cubejump/',coupon : false,mixName:'cubejump'},
+        {name:'Amazing Bros',iconName:'bros',url:'http://amazingapp.co/juegos/amazingbros/',coupon: true,mixName:'amazingbros', id:5654313976201216},
+        {name:'Zombie\n Crush',iconName:'zombie',url:'http://amazingapp.co/juegos/zombiecrush/',coupon : false,mixName:'zombiecrush',id:5769015641243648},
+        {name:'Cirquit',iconName:'cirquit',url:'http://amazingapp.co/juegos/cirquit/',coupon : false,mixName:'cirquit',id:5739719937753088},
+        {name:'Clown Rush',iconName:'clown',url:'http://amazingapp.co/juegos/clownrush/',coupon : false,mixName:'clownrush',id:5649050225344512},
+        {name:'Gravity\n Switch',iconName:'gravity',url:'http://amazingapp.co/juegos/graviswitch/',coupon : false,mixName:'graviswitch',id:5752754626625536},
+        {name:'Hexhop',iconName:'hexhop',url:'http://amazingapp.co/juegos/hexhop/',coupon : false,mixName:'hexhop',id:5153049148391424},
+        {name:'Memorama\n del Sabor',iconName:'memorama',url:'http://amazingapp.co/juegos/memorama/',coupon : false,mixName:'memorama',id:5667908084563968},
+        {name:'Jump Ward',iconName:'jump',url:'http://amazingapp.co/juegos/jumpward/',coupon : false,mixName:'jumpward',id:5647648723828736},
+        {name:'Jungle Fury',iconName:'jungle',url:'http://amazingapp.co/juegos/junglefury/',coupon : false,mixName:'junglefury',id:5644406560391168},
+        {name:'Lluvia de\n Snacks',iconName:'lluvia',url:'http://amazingapp.co/juegos/chilimbalam/',coupon : false,mixName:'chilimbalam',id:5676073085829120},
+        {name:'Store Panic',iconName:'panic',url:'http://amazingapp.co/juegos/storepanic/',coupon : false,mixName:'storepanic',id:5709436928655360},
+        {name:'Tapatopo',iconName:'tapa',url:'http://amazingapp.co/juegos/tapatopo/',coupon : false,mixName:'tapatopo',id:5664248772427776},
+        {name:'Twin Dots',iconName:'twin',url:'http://amazingapp.co/juegos/twindots/',coupon : false,mixName:'twindots',id:5750790484393984},
+        {name:'Neon Edge',iconName:'neon',url:'http://amazingapp.co/juegos/neonedge/',coupon : false,mixName:'neonedge',id:5742796208078848},
+        {name:'Cube Jump',iconName:'cube',url:'http://amazingapp.co/juegos/cubejump/',coupon : false,mixName:'cubejump',id:5674368789118976},
         
     ]
     
