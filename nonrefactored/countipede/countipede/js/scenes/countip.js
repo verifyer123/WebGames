870177f42b7c3e91eadb9cb53844af02cce64fc1
pzlewinski <pawelranger@hotmail.com>
2017-07-09 23:09:24 -0500

    countipede update

--- nonrefactored/countipede/countipede/js/scenes/countip.js
+++ nonrefactored/countipede/countipede/js/scenes/countip.js
@@ -50,6 +50,8 @@ var countip = function(){
 	}
 
 	var NUM_LIFES = 3
+	var GRID_SIZE = 60
+	var MAX_SPACE = 54
 
 	// var ROUNDS = [
 	//     {continent: "america", flags: ["mexico", "usa"]},
@@ -69,6 +71,18 @@ var countip = function(){
 	var inputsEnabled
 	var pointsBar
 	var roundCounter
+	var worm
+	var speed
+	var swipe
+	var hitBox
+	var isActive
+	var objectsInGame
+	var objectList
+	var wormGroup
+	var timeElapsed
+	var apple
+	var grid
+	var newDirection
 
 	function loadSounds(){
 		sound.decode(assets.sounds)
@@ -83,6 +97,16 @@ var countip = function(){
 		timeValue = 7
 		quantNumber = 2
 		roundCounter = 0
+		timeElapsed = 0
+		speed = 2
+		objectsInGame = []
+		objectList = []
+
+		// worm.col = 4
+		// worm.row = 5
+		// var coordinates = grid[worm.col][worm.row]
+		// worm.x = coordinates.x
+		// worm.y = coordinates.y
 
 		sceneGroup.alpha = 0
 		game.add.tween(sceneGroup).to({alpha:1},400, Phaser.Easing.Cubic.Out,true)
@@ -135,11 +159,10 @@ var countip = function(){
 	}
 
 	function createGameObjects(){
-		pullGroup = game.add.group()
-		pullGroup.x = -game.world.centerX * 2
-		pullGroup.y = -game.world.centerY * 2
-		sceneGroup.add(pullGroup)
-		pullGroup.alpha = 0
+
+		apple = wormGroup.create(0,0,"atlas.countip","apple")
+		apple.anchor.setTo(0.5, 0.5)
+		apple.kill()
 
 	}
 
@@ -186,6 +209,8 @@ var countip = function(){
 		game.load.image('introscreen',"images/countip/introscreen.png")
 		game.load.image('howTo',"images/countip/how" + localization.getLanguage() + ".png")
 		game.load.image('buttonText',"images/countip/play" + localization.getLanguage() + ".png")
+		game.load.image('box',"images/countip/box.png")
+		game.load.spine('centipede', "images/spine/centipede.json")
 
 		buttons.getImages(game)
 
@@ -208,8 +233,42 @@ var countip = function(){
 
 	}
 
-	function startRound(notStarted) {
+	// function startSnake() {
+	// 	var toCol = worm.col
+	// 	var toRow = worm.row
+	// 	switch (worm.direction) {
+	// 		case "up":
+	// 			toRow += 0
+	// 			toCol += -1
+	// 			break;
+	// 		case "down":
+	// 			toRow += 0
+	// 			toCol += 1
+	// 			break;
+	// 		case "left":
+	// 			toRow += -1
+	// 			toCol += 0
+	// 			break;
+	// 		case "right":
+	// 			toRow += 1
+	// 			toCol += 0
+	// 			break;
+	// 	}
+	// 	var nextCoordinate = grid[toCol] ? grid[toCol][toRow] : undefined
+	// 	if(nextCoordinate){
+	// 		// var move = game.add.tween(worm).to({x:nextCoordinate.x, y:nextCoordinate.y}, 500, null, true)
+	// 		// move.onComplete.add(startSnake)
+	// 		worm.col = toCol
+	// 		worm.row = toRow
+	// 		worm.x = nextCoordinate.x
+	// 		worm.y = nextCoordinate.y
+	// 	}
+	// }
 
+	function startRound(notStarted) {
+		isActive = true
+		// startSnake()
+		addRandomApple()
 
 	}
 
@@ -283,7 +342,8 @@ var countip = function(){
 		game.add.tween(tutoGroup).to({alpha:0},500,Phaser.Easing.linear,true).onComplete.add(function(){
 
 			tutoGroup.y = -game.world.height
-			startTimer(missPoint)
+			startRound()
+			// startTimer(missPoint)
 		})
 	}
 
@@ -355,12 +415,422 @@ var countip = function(){
 
 	}
 
+	function createSpine(skeleton, skin, idleAnimation, x, y) {
+		idleAnimation = idleAnimation || "IDLE"
+		var spineGroup = game.add.group()
+		x = x || 0
+		y = y || 0
+
+		var spineSkeleton = game.add.spine(0, 0, skeleton)
+		spineSkeleton.x = x; spineSkeleton.y = y
+		// spineSkeleton.scale.setTo(0.8,0.8)
+		spineSkeleton.setSkinByName(skin)
+		spineSkeleton.setAnimationByName(0, idleAnimation, true)
+		spineSkeleton.autoUpdateTransform ()
+		spineGroup.add(spineSkeleton)
+
+
+		spineGroup.setAnimation = function (animations, onComplete) {
+			var entry
+			for(var index = 0; index < animations.length; index++) {
+				var animation = animations[index]
+				var loop = index === animations.length - 1
+				if (index === 0)
+					entry = spineSkeleton.setAnimationByName(0, animation, loop)
+				else
+					spineSkeleton.addAnimationByName(0, animation, loop)
+
+			}
+			if(onComplete){
+				entry.onComplete = onComplete
+			}
+		}
+
+		spineGroup.setSkinByName = function (skin) {
+			spineSkeleton.setSkinByName(skin)
+			spineSkeleton.setToSetupPose  ()
+		}
+
+		spineGroup.setAlive = function (alive) {
+			spineSkeleton.autoUpdate = alive
+		}
+
+		spineGroup.getSlotContainer = function (slotName) {
+			var slotIndex
+			for(var index = 0, n = spineSkeleton.skeletonData.slots.length; index < n; index++){
+				var slotData = spineSkeleton.skeletonData.slots[index]
+				if(slotData.name === slotName){
+					slotIndex = index
+				}
+			}
+
+			if (slotIndex){
+				return spineSkeleton.slotContainers[slotIndex]
+			}
+		}
+
+		spineGroup.spine = spineSkeleton
+
+		return spineGroup
+	}
+	
+	function addCircleWorm() {
+		var circleGroup = game.add.group()
+		wormGroup.add(circleGroup)
+
+		var circle = game.add.graphics()
+		circle.beginFill(0xffffff)
+		circle.drawCircle(0,0, 55)
+		circle.alpha = 0.5
+		circle.endFill()
+
+		var circleSprite = game.add.sprite(0,0)
+		circleSprite.addChild(circle)
+		circleSprite.anchor.setTo(0.5, 1)
+		circleGroup.add(circleSprite)
+
+		var num = worm.circles.length - 1
+		var lastCircle = worm.circles[num]
+		lastCircle.trace = []
+
+		circleGroup.x = lastCircle.x
+		circleGroup.y = lastCircle.y
+
+		var hitbox = game.add.graphics()
+		hitbox.beginFill(0xffffff)
+		hitbox.drawRect(0,0,GRID_SIZE * 0.5, GRID_SIZE * 0.5)
+		hitbox.endFill()
+		hitbox.alpha = 0.4
+		hitbox.x = -hitbox.width * 0.5
+		hitbox.y = -hitbox.height * 0.5
+		circleGroup.hitbox = hitbox
+		circleGroup.add(hitbox)
+
+		circleGroup.direction = lastCircle.direction
+		circleGroup.traceIndex = 0
+		circleGroup.trace = []
+		worm.circles.push(circleGroup)
+
+		wormGroup.sendToBack(circleGroup)
+
+	}
+	
+	function createCountipUI() {
+
+		var boxGroup = game.add.group()
+		boxGroup.x = game.world.centerX
+		boxGroup.y = game.world.centerY
+		sceneGroup.add(boxGroup)
+
+		var box = boxGroup.create(0, 0, "box")
+		box.anchor.setTo(0.5, 0.5)
+
+		hitBox = game.add.graphics()
+		hitBox.beginFill(0xffffff)
+		hitBox.drawRect(0,0,440, 575)
+		hitBox.endFill()
+		hitBox.alpha = 0.4
+		hitBox.x = -hitBox.width * 0.5
+		hitBox.y = -hitBox.height * 0.5 + 25
+		boxGroup.add(hitBox)
+
+		wormGroup = game.add.group()
+		wormGroup.x = boxGroup.x
+		wormGroup.y = boxGroup.y
+		sceneGroup.add(wormGroup)
+
+		worm = createSpine("centipede","front", "IDLE", 0, 25)
+		worm.setAnimation(["IDLE"])
+		wormGroup.add(worm)
+		worm.direction = "down"
+		newDirection = "down"
+		worm.circles = []
+		worm.trace = []
+		worm.circles.push(worm)
+		worm.oldX = 0
+		worm.oldY = 0
+
+		worm.hitbox = game.add.graphics()
+		worm.hitbox.beginFill(0xffffff)
+		worm.hitbox.drawRect(0,0,25, 25)
+		worm.hitbox.endFill()
+		worm.hitbox.alpha = 0.4
+		worm.hitbox.x = -worm.hitbox.width * 0.5
+		worm.hitbox.y = -worm.hitbox.height * 0.5
+		worm.add(worm.hitbox)
+
+		var exit = game.add.graphics()
+		exit.beginFill(0x000000)
+		exit.drawRect(0,0,150, 100)
+		exit.endFill()
+		exit.alpha = 0.4
+		exit.x = -boxGroup.width * 0.5
+		exit.y = -boxGroup.height * 0.5 + 75
+		boxGroup.add(exit)
+		wormGroup.exit = exit
+
+
+		// addCircleWorm()
+		// addCircleWorm()
+		// addCircleWorm()
+		// addCircleWorm()
+		// addCircleWorm()
+		// addCircleWorm()
+	}
+	
+	function generateGrid() {
+		var width = hitBox.width - GRID_SIZE
+		var height = hitBox.height - GRID_SIZE
+		var numRows = Math.ceil(height / GRID_SIZE)
+		var numCols = Math.ceil(width / GRID_SIZE)
+		console.log(numRows, numCols)
+		grid = []
+
+		var startX = -width * 0.5 + 70
+		var startY = -height * 0.5 + 40
+		for(var gridIndex = 0; gridIndex < numCols * numRows; gridIndex++) {
+
+			if (gridIndex > 1) {
+				var xIndex = (gridIndex % numCols) - 1
+				var yIndex = Math.ceil((gridIndex + 1) / numCols) - 1
+
+				var x = startX + GRID_SIZE * xIndex
+				var y = startY + GRID_SIZE * yIndex
+				var coordinates = {x: x, y: y}
+
+				// var appleito = wormGroup.create(x,y,"atlas.countip","apple")
+				// appleito.anchor.setTo(0.5, 0.5)
+				// appleito.alpha = 0.4
+
+				grid.push(coordinates)
+			}
+		}
+
+	}
+	
+	function hideWorm() {
+		var toX = wormGroup.exit.x + 95
+		var toY = wormGroup.exit.y + 50
+
+		for(var circleIndex = 0; circleIndex < worm.circles.length; circleIndex++){
+			var circle = worm.circles[circleIndex]
+			var delay = (circleIndex + 1) * 100
+			var time = circleIndex * 100 + 400
+			game.add.tween(circle).to({x:toX, y:toY}, time, null, true, delay)
+			game.add.tween(circle).to({alpha:0}, time, Phaser.Easing.Cubic.In, true, delay * 2)
+		}
+	}
+	
+	function checkExit() {
+		var head = worm.circles[0]
+		var collide = checkOverlap2(worm.hitbox, wormGroup.exit)
+		if(collide){
+			isActive = false
+			hideWorm()
+		}
+	}
+
+	function checkOverlap(spriteA, spriteB) {
+
+		// var boundsA = spriteA.getBounds();
+		var boundsB = spriteB.getBounds();
+
+		return Phaser.Rectangle.intersects(spriteA , boundsB );
+
+	}
+
+	function checkOverlap2(spriteA, spriteB) {
+
+		var boundsA = spriteA.getBounds();
+		var boundsB = spriteB.getBounds();
+
+		return Phaser.Rectangle.intersects(boundsA , boundsB );
+
+	}
+	
+	function checkCollision(obj) {
+		var collide = false
+		for(var circleIndex = 0, num = worm.circles.length; circleIndex < num; circleIndex++){
+			var circle = worm.circles[circleIndex]
+
+			collide = checkOverlap(obj, circle.hitbox)
+			if(collide)
+				break;
+		}
+		return collide
+	}
+	
+	function addRandomApple() {
+
+		isActive = false
+		apple.alpha = 0
+
+		grid = Phaser.ArrayUtils.shuffle(grid)
+		var randomElement = grid[0]
+
+		var x = randomElement.x
+		var y = randomElement.y
+		var worldX = x + wormGroup.x - GRID_SIZE
+		var worldY = y + wormGroup.y - GRID_SIZE
+		var checkRect = new Phaser.Rectangle(worldX, worldY, GRID_SIZE * 2, GRID_SIZE * 2)
+
+		// var rect = game.add.graphics()
+		// rect.beginFill(0x000000)
+		// rect.drawRect(worldX, worldY, GRID_SIZE * 2, GRID_SIZE * 2)
+		// rect.endFill()
+		// sceneGroup.add(rect)
+
+		var collide = checkCollision(checkRect, true)
+		if(collide){
+			addRandomApple()
+		}else {
+			isActive = true
+			apple.alpha = 1
+			apple.x = x, apple.y = y
+			apple.revive()
+		}
+
+	}
+	
+	function selfCollision() {
+		var head = worm.circles[0]
+		var collide = false
+		for(var circleIndex = 1; circleIndex < worm.circles.length; circleIndex++){
+			var circle = worm.circles[circleIndex]
+			collide = checkOverlap2(circle.hitbox, head.hitbox)
+			if ((collide)&&(circle.trace.length > 0)){
+				isActive = false
+				worm.setAnimation(["LOSE"])
+				break;
+			}
+
+		}
+		return collide
+	}
+	
+	function checkBounds() {
+		var rectangle = hitBox.getBounds()
+		// rectangle.inflate(20, 20)
+		var rectangle2 = worm.hitbox.getBounds()
+		var contains = rectangle.containsRect(rectangle2)
+
+		if(!contains){
+			isActive = false
+			worm.setAnimation(["LOSE"])
+		}
+	}
+	
+	// function changeDirection(circle, lastCircle) {
+	// 	circle.direction = lastCircle.direction
+	// }
+	
+	function updateTail() {
+		for(var circleIndex = 1, num = worm.circles.length; circleIndex < num; circleIndex++){
+			var circle = worm.circles[circleIndex]
+			var pastCircle = worm.circles[circleIndex - 1]
+
+			var spaceTrace = Math.round(MAX_SPACE / speed)
+			if(pastCircle.trace[spaceTrace]){
+				var trace = pastCircle.trace[circle.traceIndex]
+				circle.x = trace.x, circle.y = trace.y
+				circle.traceIndex++
+
+				var trace = {x:circle.x, y:circle.y}
+				circle.trace.push(trace)
+			}
+		}
+	}
+	
+	function update() {
+
+		var direction = swipe.check()
+		if (direction !== null) {
+			switch (direction.direction) {
+				case swipe.DIRECTION_UP:
+					if(worm.direction !== "down"){
+						worm.direction = "up"
+						worm.setSkinByName("bot")
+					}
+					break;
+				case swipe.DIRECTION_DOWN:
+					if(worm.direction !== "up") {
+						worm.direction = "down"
+						worm.setSkinByName("front")
+					}
+					break;
+				case swipe.DIRECTION_LEFT:
+					if(worm.direction !== "right") {
+						worm.direction = "left"
+						worm.scale.x = 1
+						worm.setSkinByName("lefth&rigth")
+					}
+					break;
+				case swipe.DIRECTION_RIGHT:
+					if(worm.direction !== "left") {
+						worm.direction = "right"
+						worm.scale.x = -1
+						worm.setSkinByName("lefth&rigth")
+					}
+					break;
+			}
+		}
+
+		// if(worm.direction !== newDirection){
+		// 	var difX = Math.abs(worm.oldX - worm.x)
+		// 	var difY = Math.abs(worm.oldY - worm.y)
+		// 	if((difX >= GRID_SIZE)||(difY >= GRID_SIZE)) {
+		// 		worm.direction = newDirection
+		// 		worm.oldX = worm.x
+		// 		worm.oldY = worm.y
+		// 	}
+		// }
+
+		if(worm.direction === "down"){
+			worm.y += speed
+		}else if(worm.direction === "up"){
+			worm.y -= speed
+		}else if(worm.direction === "left"){
+			worm.x -= speed
+		}else if(worm.direction === "right"){
+			worm.x += speed
+		}
+
+		var trace = {x:worm.x, y:worm.y}
+		worm.trace.push(trace)
+
+		updateTail()
+		checkBounds()
+
+		// timeElapsed += game.time.elapsedMS
+		var x = apple.world.x - apple.width * 0.5
+		var y = apple.world.y - apple.height * 0.5
+		var checkRect = new Phaser.Rectangle(x, y, apple.width, apple.height)
+
+		var collision = checkCollision(checkRect)
+		if(apple.alive) {
+			if (collision) {
+				apple.kill()
+				addCircleWorm()
+				addRandomApple()
+			}
+		}
+
+		selfCollision()
+		checkExit()
+
+	}
+
 	return {
 		assets: assets,
 		name: "countip",
 		preload:preload,
+		update:function(){
+			if(isActive)
+				update()
+		},
 		create: function(event){
 
+			swipe = new Swipe(game)
 			sceneGroup = game.add.group()
 
 			var background = sceneGroup.create(-2,-2,'fondo')
@@ -369,6 +839,7 @@ var countip = function(){
 
 			var swatch = game.add.tileSprite(0,0,game.world.width, game.world.height, "atlas.countip", "swatch")
 			sceneGroup.add(swatch)
+			swatch.alpha = 0.7
 
 			countipSong = game.add.audio('countipSong')
 			game.sound.setDecodedCallback(countipSong, function(){
@@ -383,14 +854,17 @@ var countip = function(){
 				game.sound.mute = false
 			}, this);
 
+			createCountipUI()
+			generateGrid()
 			initialize()
 
 			createHearts()
 			createPointsBar()
 			createGameObjects()
-			createClock()
-			startRound(true)
+			// createClock()
 			createTutorial()
+
+			// addRandomApple()
 
 			buttons.getButton(countipSong,sceneGroup,game.world.width - 50)
 		}
