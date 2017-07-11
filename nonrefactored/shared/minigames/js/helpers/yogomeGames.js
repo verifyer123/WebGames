49b5b40b0b265d28d7515a4dbfc74c50aaf6760d
pzlewinski <pawelranger@hotmail.com>
2017-07-11 13:13:04 -0500

    yogomeGames update

--- nonrefactored/shared/minigames/js/helpers/yogomeGames.js
+++ nonrefactored/shared/minigames/js/helpers/yogomeGames.js
@@ -57,7 +57,8 @@ yogomeGames.getGames = function(){
         {name:'luckynumber',url:'http://yogome.com/epic/minigames/luckynumbersite/',sceneName:'luckynumber',subject:'math', review:false},//50
         {name:'robovet',url:'http://yogome.com/epic/minigames/roboSite/',sceneName:'robo',subject:'math', review:false},//51
 		{name:'mathBomb',url:'http://yogome.com/epic/minigames/bombSite/',sceneName:'bomb',subject:'math', review:false},//52
-		{name:'uniDream',url:'http://yogome.com/epic/minigames/uniSite/',sceneName:'uni',subject:'math', review:false}//53
+		{name:'uniDream',url:'http://yogome.com/epic/minigames/uniSite/',sceneName:'uni',subject:'math', review:false},//53
+		{name:'countipede',url:'http://yogome.com/epic/minigames/countipSite/',sceneName:'countip',subject:'math', review:false}//54
 	]
 
 	
