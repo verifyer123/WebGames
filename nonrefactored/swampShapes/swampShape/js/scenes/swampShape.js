5b5196e58cd1374b8789998d59196835517c2336
pzlewinski <pawelranger@hotmail.com>
2017-07-26 16:36:41 -0500

    swampShapes completed

--- nonrefactored/swampShapes/swampShape/js/scenes/swampShape.js
+++ nonrefactored/swampShapes/swampShape/js/scenes/swampShape.js
@@ -12,31 +12,29 @@ var swampShape = function(){
 			}],
         images: [],
 		sounds: [
-            {	name: "pop",
+			{	name: "pop",
 				file: soundsPath + "pop.mp3"},
-            {	name: "magic",
+			{	name: "magic",
 				file: soundsPath + "magic.mp3"},
-            {	name: "wrong",
-				file: soundsPath + "wrong.mp3"},
-            {	name: "whoosh",
-				file: soundsPath + "whoosh.mp3"},
-            {	name: "gameLose",
-				file: soundsPath + "gameLose.mp3"},
-            {	name: "wrongItem",
-				file: soundsPath + "wrongItem.mp3"},
-            {	name: "break",
-				file: soundsPath + "glassbreak.mp3"},
-            {	name: "powerup",
-				file: soundsPath + "powerup.mp3"},
-            {	name: "balloon",
-				file: soundsPath + "inflateballoon.mp3"},
-			{	name: "explode",
-				file: soundsPath + "explode.mp3"},
-			{	name: "shootBall",
-				file: soundsPath + "shootBall.mp3"},
+			{	name: "cut",
+				file: soundsPath + "cut.mp3"},
 			{	name: "combo",
-				file: soundsPath + "combo.mp3"}
-		],
+				file: soundsPath + "combo.mp3"},
+			{	name: "flip",
+				file: soundsPath + "flipCard.mp3"},
+			{	name: "swipe",
+				file: soundsPath + "swipe.mp3"},
+			{	name: "wrong",
+				file: soundsPath + "wrong.mp3"},
+			{	name: "right",
+				file: soundsPath + "rightChoice.mp3"},
+			{   name: "gameLose",
+				file: soundsPath + "gameLose.mp3"},
+			{   name: "fireFly",
+				file: soundsPath + "fliege.mp3"},
+			{   name: "insectLaghing",
+				file: soundsPath + "insectLaghing.mp3"}
+		]
 	}
 
 	var MAX_STARTS = 8
@@ -60,16 +58,20 @@ var swampShape = function(){
 			correctIndex: [{x:2, y:0}, {x:-1, y:-2}, {x:-1, y:2}]
 		},
 		{
-			name:"rectangleH",
+			name:"rectangle",
 			correctIndex: [{x:2, y:0}, {x:0, y:-1}, {x:-2, y:0}, {x:0, y:1}]
 		},
 		{
-			name:"rectangleV",
+			name:"rectangle2",
 			correctIndex:[{x:1, y:0}, {x:0, y:-2}, {x:-1, y:0}, {x:0, y:2}]
 		},
 		{
-			name:"triangleIso",
-			correctIndex: [{x:2, y:0}, {x:-1, y:-2}, {x:-1, y:2}]
+			name:"isoceles",
+			correctIndex: [{x:2, y:0}, {x:-1, y:-1}, {x:-1, y:1}]
+		},
+		{
+			name:"rhombus",
+			correctIndex: [{x:1, y:-1}, {x:-1, y:-1}, {x:-1, y:1}, {x:1, y:1}]
 		},
 		// {
 		// 	name:"trapezoid",
@@ -89,16 +91,18 @@ var swampShape = function(){
 	]
 
 	var ROUNDS = [
-		{figure:"triangle"}
+		{figure:"square"},
+		{figure:"triangle"},
+		{figure:"rhombus"},
+		{figure:"rectangle"},
+		{figure:"rectangle2"},
+		{figure:"isoceles"},
+		{figure:"random"}
 	]
 
 	var COORDINATES = [{x:-180, y:-180}, {x:0, y:-180}, {x: 180, y:-180}, {x: 180, y:0}, {x:180, y:180}, {x:0, y:180}, {x:-180, y:180}, {x:-180, y:0}]
 	
-	var sceneGroup = null;
-	var heartsIcon;
-	var lives = 1;
-	var coins = 0;
-    var shapesBottle = new Array;
+	var sceneGroup = null
     var bottle;
     var pointerGame
 	var wrongParticle
@@ -106,7 +110,6 @@ var swampShape = function(){
 	var currentLine
 	var lines
 	var canvasGroup
-	var flyList
 	var starsGroup
 	var figuresList
 	var tutoGroup
@@ -115,14 +118,21 @@ var swampShape = function(){
 	var flysInGame
 	var isActive
 	var currentFigure
+	var pointsBar
+	var astronoSong
+	var clock
+	var timeValue
 
-    function preload() {
-		game.load.audio('sillyAdventureGameLoop',  soundsPath + 'songs/sillyAdventureGameLoop.mp3');
+	var gameIndex = 64
+
+	function preload() {
+		game.load.audio('astronoSong',  soundsPath + 'songs/sillyAdventureGameLoop.mp3');
 		/*Default*/
 		buttons.getImages(game);
 		game.load.image('introscreen',"images/swampShape/introscreen.png")
 		game.load.image('howTo',"images/swampShape/how" + localization.getLanguage() + ".png")
 		game.load.image('buttonText',"images/swampShape/play" + localization.getLanguage() + ".png")
+		buttons.getImages(game)
 		
 		/*GAME*/
 		// game.load.image("background",imagePath + "background.png");
@@ -148,9 +158,9 @@ var swampShape = function(){
 		// quantNumber = 2
 		roundCounter = 0
 		figuresList = []
-		flyList = []
 		flysInGame = []
 		lines = []
+		timeValue = 12
 		// isActive = false
 
 		sceneGroup.alpha = 0
@@ -170,27 +180,124 @@ var swampShape = function(){
 
 	}
 	
-	function recursiveSumResult(finalResults) {
-		var checkLine = lines.pop()
+	function recursiveSumResult(copyLines, finalResults) {
+		var checkLine = copyLines.pop()
 		var sumResult = checkLine.result
-		for(var lineIndex = lines.length - 1; lineIndex >= 0; lineIndex--){
-			var line = lines[lineIndex]
+		for(var lineIndex = copyLines.length - 1; lineIndex >= 0; lineIndex--){
+			var line = copyLines[lineIndex]
 			if((checkLine.result.x === line.result.x)&&(checkLine.result.y === line.result.y)){
 				// console.log("equal")
 				sumResult.x += line.result.x
 				sumResult.y += line.result.y
-				lines.splice(lineIndex, 1)
+				copyLines.splice(lineIndex, 1)
 			}
 		}
 		// console.log(sumResult)
 		finalResults.push(sumResult)
-		if(lines.length>0)
-			recursiveSumResult(finalResults)
+		if(copyLines.length>0)
+			recursiveSumResult(copyLines, finalResults)
+	}
+	
+	function hideFireflies() {
+
+		for(var lineIndex = 0; lineIndex < lines.length; lineIndex++){
+			var line = lines[lineIndex]
+			correctParticle.x = line.star.world.x
+			correctParticle.y = line.star.world.y
+			correctParticle.start(true, 1000, null, 3)
+			line.star.selected = true
+			if(line.star.tween)
+				line.star.tween.stop()
+			if(line.star.tweenScale)
+				line.star.tweenScale.stop()
+			game.add.tween(line.star.scale).to({x: 1.5, y:1.5}, 300, Phaser.Easing.Sinusoidal.In, true).yoyo(true)
+			game.add.tween(line).to({alpha:0}, 300, Phaser.Easing.Cubic.Out, true, 600)
+			game.add.tween(line.star).to({x:bottle.x - starsGroup.x, y:bottle.y - starsGroup.y}, 800, Phaser.Easing.Cubic.Out, true, 600)
+		}
+
+		for(var flyIndex = 0; flyIndex < flysInGame.length; flyIndex++){
+			var firefly = flysInGame[flyIndex]
+			if(!firefly.selected){
+				if(firefly.tween)
+					firefly.tween.stop()
+				if(firefly.tweenScale)
+					firefly.tweenScale.stop()
+				game.add.tween(firefly).to({alpha:0, x:0, y:0}, 500, Phaser.Easing.Cubic.Out, true, 1200)
+			}
+		}
+		game.add.tween(currentFigure.sprite).to({alpha: 0}, 300, Phaser.Easing.Sinusoidal.In, true, 1200)
+		game.add.tween(currentFigure.sprite.scale).to({x: 0.4, y:0.4}, 300, Phaser.Easing.Sinusoidal.In, true, 1200)
+
+		game.time.events.add(2000, function () {
+			clearLines()
+			startRound()
+		})
+
+		game.time.events.add(600, function () {
+			sound.play("fireFly")
+		})
+	}
+
+	function addNumberPart(obj,number){
+
+		var fontStyle = {font: "38px VAGRounded", fontWeight: "bold", fill: "#ffffff", align: "center"}
+
+		var pointsText = new Phaser.Text(sceneGroup.game, 0, 5, number, fontStyle)
+		pointsText.x = obj.world.x
+		pointsText.y = obj.world.y
+		pointsText.anchor.setTo(0.5,0.5)
+		sceneGroup.add(pointsText)
+
+		game.add.tween(pointsText).to({y:pointsText.y + 100},800,Phaser.Easing.linear,true)
+		game.add.tween(pointsText).to({alpha:0},250,Phaser.Easing.linear,true,500)
+
+		pointsText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 0);
+
+	}
+
+	function createClock(){
+
+		clock = game.add.group()
+		clock.x = game.world.centerX
+		clock.y = 80
+		sceneGroup.add(clock)
+
+		var clockImage = clock.create(0,0,'atlas.swampShape','clock')
+		clockImage.anchor.setTo(0.5,0.5)
+
+		var clockBar = clock.create(-clockImage.width* 0.38,19,'atlas.swampShape','bar')
+		clockBar.anchor.setTo(0,0.5)
+		clockBar.width = clockImage.width*0.76
+		clockBar.height = 22
+		clockBar.origScale = clockBar.scale.x
+
+		clock.bar = clockBar
+
+	}
+
+	function addPoint(number){
+
+		sound.play("magic")
+		pointsBar.number+=number;
+		pointsBar.text.setText(pointsBar.number)
+
+		var scaleTween = game.add.tween(pointsBar.scale).to({x: 1.05,y:1.05}, 200, Phaser.Easing.linear, true)
+		scaleTween.onComplete.add(function(){
+			game.add.tween(pointsBar.scale).to({x: 1,y:1}, 200, Phaser.Easing.linear, true)
+		})
+
+		addNumberPart(pointsBar.text,'+' + number)
+
+		// if(pointsBar.number % 2 == 0){
+		timeValue-=timeValue * 0.08
+		// }
+
 	}
 	
 	function checkFigure() {
 		var finalResults = []
-		recursiveSumResult(finalResults)
+		var copyArray = lines.slice()
+		recursiveSumResult(copyArray, finalResults)
 		console.log(finalResults)
 
 		var isRight = true
@@ -208,7 +315,18 @@ var swampShape = function(){
 			isRight = isRight && check
 		}
 		
-		console.log("isSquare? ", isRight)
+		if(isRight){
+			addPoint(1)
+			hideFireflies()
+		}else{
+			for(var lineIndex = 0; lineIndex < lines.length; lineIndex++){
+				var line = lines[lineIndex]
+				wrongParticle.x = line.star.world.x
+				wrongParticle.y = line.star.world.y
+				wrongParticle.start(true, 1000, null, 3)
+			}
+			stopGame()
+		}
 	}
 
 	function checkLines(){
@@ -224,18 +342,13 @@ var swampShape = function(){
 			console.log("complete")
 			checkFigure()
 			isActive = false
+			if (clock.tween)
+				clock.tween.stop()
+
 		}
 	}
 
 	function createGameObjects(){
-		for(var figureIndex = 0; figureIndex < FIGURES.length; figureIndex++){
-			var figureData = FIGURES[figureIndex]
-
-			var figure = sceneGroup.create(0, -40, "atlas.swampShape", figureData.name)
-			figure.anchor.setTo(0.5, 0.5)
-			figure.kill()
-			figuresList[figureData.name] = {sprite: figure, figureData: figureData}
-		}
 
 		canvasGroup = game.add.group()
 		sceneGroup.add(canvasGroup)
@@ -248,14 +361,14 @@ var swampShape = function(){
 		for(var starIndex = 0; starIndex < COORDINATES.length; starIndex++){
 			var firefly = starsGroup.create(0,0, "atlas.swampShape", "firefly")
 			firefly.anchor.setTo(0.5, 0.5)
-			var coordinate = COORDINATES[starIndex]
+			// var coordinate = COORDINATES[starIndex]
 			// firefly.setAnimation(["IDLE_1"])
-			flyList.push(firefly)
 			// firefly.sprite = game.add.sprite(0,0)
 			// firefly.add(firefly.sprite)
 
 			// firefly.alpha = 0
-			firefly.x = coordinate.x; firefly.y = coordinate.y
+			firefly.x = 0; firefly.y = 0
+			firefly.alpha = 0
 			firefly.index = starIndex
 			flysInGame.push(firefly)
 		}
@@ -273,6 +386,29 @@ var swampShape = function(){
 
 	}
 
+	function createPointsBar(){
+
+		pointsBar = game.add.group()
+		pointsBar.x = game.world.width
+		pointsBar.y = 0
+		sceneGroup.add(pointsBar)
+
+		var pointsImg = pointsBar.create(-10,10,'atlas.swampShape','xpcoins')
+		pointsImg.anchor.setTo(1,0)
+
+		var fontStyle = {font: "35px VAGRounded", fontWeight: "bold", fill: "#ffffff", align: "center"}
+		var pointsText = new Phaser.Text(sceneGroup.game, 0, 0, "0", fontStyle)
+		pointsText.x = -pointsImg.width * 0.45
+		pointsText.y = pointsImg.height * 0.25
+		pointsBar.add(pointsText)
+
+		pointsText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 0);
+
+		pointsBar.text = pointsText
+		pointsBar.number = 0
+
+	}
+
 	/*CREATE SCENE*/
     function createScene() {
 		sceneGroup = game.add.group();
@@ -280,14 +416,8 @@ var swampShape = function(){
 		game.physics.startSystem(Phaser.Physics.P2JS);
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 
-		var background = game.add.tileSprite(0, 0, game.width, game.height, 'atlas.swampShape', "swamp-03");
+		var background = game.add.tileSprite(0, 0, game.width, game.height, 'atlas.swampShape', "background");
 		sceneGroup.add(background);
-
-		bottle = sceneGroup.create(0, 0, "atlas.swampShape", "swamp-05");
-		bottle.anchor.setTo(0.5, 0.5);
-		bottle.x = game.world.centerX;
-		bottle.y = game.height - bottle.height / 1.8;
-		bottle.id = 0;
 
 		var rectangle = game.add.graphics()
 		rectangle.beginFill(0xffffff)
@@ -312,9 +442,40 @@ var swampShape = function(){
 		sceneGroup.add(pointerGame)
 		graphic.destroy()
 
+		astronoSong = game.add.audio('astronoSong')
+		game.sound.setDecodedCallback(astronoSong, function(){
+			astronoSong.loopFull(0.6)
+		}, this);
+
+		game.onPause.add(function(){
+			game.sound.mute = true
+		} , this);
+
+		game.onResume.add(function(){
+			game.sound.mute = false
+		}, this);
+
 		initialize()
 
 		createGameObjects()
+
+		bottle = sceneGroup.create(0, 0, "atlas.swampShape", "bottle");
+		bottle.anchor.setTo(0.5, 0.5);
+		bottle.x = game.world.centerX;
+		bottle.y = game.height - bottle.height / 1.8;
+		bottle.id = 0;
+
+		for(var figureIndex = 0; figureIndex < FIGURES.length; figureIndex++){
+			var figureData = FIGURES[figureIndex]
+
+			var figure = sceneGroup.create(bottle.x, bottle.y + 18, "atlas.swampShape", figureData.name)
+			figure.anchor.setTo(0.5, 0.5)
+			figure.kill()
+			figuresList[figureData.name] = {sprite: figure, figureData: figureData}
+		}
+
+		createClock()
+		createPointsBar()
 		createTutorial()
 
 		correctParticle = createPart("star")
@@ -326,6 +487,7 @@ var swampShape = function(){
 		// wrongParticle.x = signGroup.x
 		// wrongParticle.y = signGroup.y
 		sceneGroup.add(wrongParticle)
+		buttons.getButton(astronoSong,sceneGroup)
 
 	}
 
@@ -425,7 +587,7 @@ var swampShape = function(){
 			currentLine.clear()
 			currentLine = null
 		}else{
-			// sound.play("cut")
+			sound.play("cut")
 			lines.push(currentLine)
 			console.log(lines.length)
 		}
@@ -476,8 +638,68 @@ var swampShape = function(){
 
 	}
 
+	function stopGame(win){
+
+		//objectsGroup.timer.pause()
+		//timer.pause()
+		// wrongParticle.start(true, 1000, null, 5)
+		astronoSong.stop()
+		// clock.tween.stop()
+		isActive = false
+		sound.play("wrong")
+		game.time.events.add(800, function () {
+			sound.play("insectLaghing")
+		})
+
+		var tweenScene = game.add.tween(sceneGroup).to({alpha: 0}, 500, Phaser.Easing.Cubic.In, true, 2000)
+		tweenScene.onComplete.add(function(){
+
+			var resultScreen = sceneloader.getScene("result")
+			resultScreen.setScore(true, pointsBar.number, gameIndex)
+
+			//amazing.saveScore(pointsBar.number)
+			sceneloader.show("result")
+			sound.play("gameLose")
+		})
+	}
+
+	function startTimer(onComplete) {
+		var delay = 500
+		// clock.bar.scale.x = clock.bar.origScale
+		if (clock.tween)
+			clock.tween.stop()
+
+
+		clock.tween = game.add.tween(clock.bar.scale).to({x:0},timeValue * 1000,Phaser.Easing.linear,true )
+		clock.tween.onComplete.add(function(){
+			if(isActive) {
+				onComplete()
+			}
+		})
+	}
+
 	function startRound(notStarted) {
+
+		game.add.tween(clock.bar.scale).to({x: clock.bar.origScale}, 300, Phaser.Easing.Cubic.Out, true)
+		sound.play("swipe")
+
+		for(var flyIndex = 0; flyIndex < flysInGame.length; flyIndex++){
+			var firefly = flysInGame[flyIndex]
+			var coordinate = COORDINATES[flyIndex]
+			firefly.selected = false
+
+			firefly.x = 0; firefly.y = 0; firefly.alpha = 0
+			firefly.scale.x = 1; firefly.scale.y = 1
+			var appear = game.add.tween(firefly).to({alpha:1, x:coordinate.x, y:coordinate.y}, 500, Phaser.Easing.Cubic.Out, true)
+			appear.onComplete.add(function (obj) {
+				var delay = game.rnd.integerInRange(100, 600)
+				obj.tween = game.add.tween(obj).to({alpha:0.6}, 300, Phaser.Easing.Sinusoidal.InOut, true, delay).yoyo(true).loop(true)
+				obj.tweenScale = game.add.tween(obj.scale).to({x:0.8, y:0.8}, 600, Phaser.Easing.Sinusoidal.InOut, true, delay).yoyo(true).loop(true)
+			})
+		}
+
 		var round = ROUNDS[roundCounter]
+		roundCounter = roundCounter + 1 < ROUNDS.length ? roundCounter + 1 : roundCounter
 
 		if(round.figure === "random"){
 			var rndFigure = FIGURES[game.rnd.integerInRange(0, FIGURES.length - 1)]
@@ -486,27 +708,16 @@ var swampShape = function(){
 		else
 			currentFigure = figuresList[round.figure]
 
-		// game.add.tween(nameGroup).to({alpha:0}, 500, Phaser.Easing.Cubic.Out, true)
-		// game.add.tween(nameGroup.scale).to({x:0.4, y:0.4}, 500, Phaser.Easing.Cubic.Out, true)
-		// var round = ROUNDS[roundCounter]
-		// roundCounter = roundCounter + 1 < ROUNDS.length ? roundCounter + 1 : roundCounter
-		//
-		// game.add.tween(clock.bar.scale).to({x: clock.bar.origScale}, 300, Phaser.Easing.Cubic.Out, true)
-		// for(var starIndex = 0; starIndex < starsInGame.length; starIndex++){
-		// 	var star = starsInGame[starIndex]
-		// 	star.setAnimation(["IDLE_1"])
-		// 	star.line = null
-		// 	star.lineIndex = null
-		// }
-		// lines = []
-		// starsInGame = []
+		currentFigure.sprite.alpha = 0
+		currentFigure.sprite.scale.x = 0.4; currentFigure.sprite.scale.y = 0.4
+		game.add.tween(currentFigure.sprite).to({alpha:1}, 500, Phaser.Easing.Cubic.Out, true)
+		game.add.tween(currentFigure.sprite.scale).to({x:1, y:1}, 500, Phaser.Easing.Cubic.Out, true)
+		currentFigure.sprite.revive()
 
-		// generateFigure(round)
-		// game.time.events.add(800, function () {
-		// 	isActive = true
-		// 	startTimer(stopGame)
-		// })
-		isActive = true
+		game.time.events.add(800, function () {
+			isActive = true
+			startTimer(stopGame)
+		})
 	}
 
 
