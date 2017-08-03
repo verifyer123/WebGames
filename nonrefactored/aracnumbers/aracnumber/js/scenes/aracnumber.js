d3a52ebb8a17a5a41c8322b4169d89ed35a7daa0
DiegoFRM <diego@yogome.com>
2017-08-03 14:23:07 -0500

    aracNumber update

--- nonrefactored/aracnumbers/aracnumber/js/scenes/aracnumber.js
+++ nonrefactored/aracnumbers/aracnumber/js/scenes/aracnumber.js
@@ -46,15 +46,26 @@ var aracnumber = function(){
 	var background;
 	var heartsGroup = null;
 	var heartsIcon;
-	var heartsText;	
+	//var heartsText;	
 	var xpIcon;
-	var xpText;
+	//var xpText;
 	var lives = 1;
 	var cursors;
 	var coins = 0;
-	var bgm = null;
+	//var bgm = null;
 	var activeGame = true;
     var gameIndex = 60;
+    var fly1;
+    var fly2;
+    var textFly1;
+    var textFly2;
+    var multiples_5 = [];
+    var multiples_10 = [];
+    var multiples_100 = [];  
+    var differentsMultiples = [100,10,5];
+    var NumberSelect = 0;
+    var activeMultiple = 100;
+    var counter = 0;
 
 	
 	styleWhite = {font: "40px VAGRounded", fontWeight: "bold", fill: "#ffffff", align: "center"};
@@ -82,19 +93,59 @@ var aracnumber = function(){
         game.load.image("rama",imagePath + "rama.png");
         game.load.image("ramas",imagePath + "ramas.png");
         game.load.image("web",imagePath + "web.png");
+        game.load.image("star",imagePath + "star.png");
+        game.load.image("wrong",imagePath + "wrong.png");
 
 		/*SPINE*/
 		game.load.spine("spider", imagePath + "spine/spider/spider.json");
         game.load.spine("fly", imagePath + "spine/fly/fly.json");
 
-		
-		
-		
 	}
+    
+    function multiple(valor, multiple)
+                {
+                    resto = valor % multiple;
+                    if(resto==0)
+                        return true;
+                    else
+                        return false;
+                }        
+    
+        function multiples(max,Number,Array){       
+        
+                 for(var i=1;i<=max;i++)
+                {
+
+                    if(multiple(i,Number))
+                        Array.push(i);
+                }
+            
+            return Array
+        }    
 
 	function loadSounds(){
 		sound.decode(assets.sounds)
 	}
+    
+    function getRandomArbitrary(min, max) {
+  		return Math.floor(Math.random() * (max - min) + min);
+	}    
+        
+    
+    
+    function getSlotContainer (spineSkeleton, slotName) {
+			var slotIndex
+			for(var index = 0, n = spineSkeleton.skeletonData.slots.length; index < n; index++){
+				var slotData = spineSkeleton.skeletonData.slots[index]
+				if(slotData.name === slotName){
+					slotIndex = index    
+				}
+			}
+
+			if (slotIndex){     
+				return spineSkeleton.slotContainers[slotIndex]
+			}
+		}    
 	
 	function initialize(){
 		lives = 1;
@@ -112,6 +163,21 @@ var aracnumber = function(){
 		game.physics.startSystem(Phaser.Physics.P2JS);
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 
+
+        
+        multiples(100,5,multiples_5)
+        multiples(100,10,multiples_10)
+        multiples(1000,100,multiples_100)
+        
+        //shuffle(multiples_100);
+        
+        console.log(multiples_5);
+        console.log(multiples_10);
+        console.log(multiples_100);
+        
+        
+        
+        
 		var background = game.add.tileSprite(0,0,game.width,game.height,"background");
 		sceneGroup.add(background);
          
@@ -147,76 +213,281 @@ var aracnumber = function(){
         var cuerda = spiderGroup.create(0,0,"cuerda");
         cuerda.anchor.setTo(0.5,0);
         cuerda.x = game.world.centerX;
+        cuerda.posx = cuerda.x;
         var spider = game.add.spine(0,0,"spider");
         spider.setAnimationByName(0, "IDLE", true);
 		spider.setSkinByName("normal");
         spider.x = game.world.centerX;
         spider.y = game.world.centerY + spider.height/1.5;
+        spider.posx = spider.x;
         spiderGroup.add(spider);
-        var textSpider = game.add.text(0, 0,"10", styleSpider,spiderGroup);
+        var textSpider = game.add.text(0, 0,activeMultiple, styleSpider,spiderGroup);
         textSpider.anchor.setTo(0.5,0.5);
-        textSpider.x = spider.x;
-        textSpider.y = spider.y - spider.height/3;
-        TweenMax.fromTo(textSpider,0.3,{y:spider.y - spider.height/3},{y:spider.y - spider.height/3.1,yoyo:true,repeat:-1});
+        var slotNumberSpider = getSlotContainer(spider,"empty");
+        slotNumberSpider.add(textSpider);
+
         spiderGroup.x = game.world.centerX - game.width/2;
         spiderGroup.posx = spiderGroup.x;
+        
         sceneGroup.add(spiderGroup);
         TweenMax.fromTo(spiderGroup,2,{x:spiderGroup.posx - 20},{x:spiderGroup.posx + 10,yoyo:true,repeat:-1});
         
         
         var flyGroup1 = game.add.group();
         
-        var fly1 = game.add.spine(0,0,"fly");
+        fly1 = game.add.spine(0,0,"fly");
         fly1.setAnimationByName(0, "IDLE", true);
 		fly1.setSkinByName("normal");
         fly1.x = game.width/3 - fly1.width/2;
-        fly1.y = game.world.centerY;
-        flyGroup1.add(fly1);        
-        var textFly1 = game.add.text(0, 0,"10", styleBlack,flyGroup1);
+        fly1.y = -200;
+        fly1.active = true;
+        fly1.value = multiples_100[NumberSelect];
+        flyGroup1.add(fly1);   
+        textFly1 = game.add.text(0, 0,fly1.value, styleBlack,flyGroup1);
         textFly1.anchor.setTo(0.5,0.5);
-        textFly1.x = fly1.x;
-        textFly1.y = fly1.y + 3;
-        TweenMax.fromTo(textFly1,0.5,{y:fly1.y+3},{y:fly1.y+8,yoyo:true,repeat:-1});
-       
+        var slotNumberFly1 = getSlotContainer(fly1,"empty");
+        slotNumberFly1.add(textFly1);
+
         sceneGroup.add(flyGroup1);
         
         var flyGroup2 = game.add.group();
         
-        var fly2 = game.add.spine(0,0,"fly");
+        fly2 = game.add.spine(0,0,"fly");
         fly2.setAnimationByName(0, "IDLE", true);
 		fly2.setSkinByName("normal");
         fly2.x = game.width - game.width/3 + fly2.width/2;
-        fly2.y = game.world.centerY;
+        fly2.posx = fly2.x;
+        fly2.y = -200;
+        fly2.active = true;    
+        fly2.value = multiples_5[NumberSelect];
         flyGroup2.add(fly2);
-        
-        var textFly2 = game.add.text(0, 0,"1000", styleBlack,flyGroup2);
-        textFly2.anchor.setTo(0.5,0.5);
-        textFly2.x = fly2.x;
-        textFly2.y = fly2.y;
-        TweenMax.fromTo(textFly2,0.5,{y:fly2.y+3},{y:fly2.y+8,yoyo:true,repeat:-1});
         sceneGroup.add(flyGroup2);
         
+        textFly2 = game.add.text(0, 0,multiples_100[NumberSelect], styleBlack,flyGroup2);
+        textFly2.anchor.setTo(0.5,0.5);     
+        var slotNumberFly2 = getSlotContainer(fly2,"empty");
+        slotNumberFly2.add(textFly2);
         
-       /* var buttonLeft = new Phaser.Graphics(game);
+        
+        var star = sceneGroup.create(0,0,"star");
+        star.anchor.setTo(0.5,0.5);
+        star.alpha = 0;
+        
+
+        
+       var buttonLeft = new Phaser.Graphics(game);
         buttonLeft.beginFill(0x000000)
         buttonLeft.drawRect(0,0,game.world.width/2, game.height)
-        buttonLeft.alpha = 0.7
+        buttonLeft.alpha = 0
         buttonLeft.endFill()
-        buttonLeft.inputEnabled = true
+        buttonLeft.inputEnabled = true;
+        sceneGroup.add(buttonLeft);
         buttonLeft.events.onInputDown.add(function(){
-        });*/
+            TweenMax.fromTo(spider,0.5,{x:spider.posx},{x:spider.posx - 100,onComplete:backSpider});
+            TweenMax.fromTo(cuerda,0.5,{x:cuerda.posx},{x:cuerda.posx - 100});
+            if(fly1.active){
+                if(fly1.y >= eval(spider.y - spider.height/2) && fly1.y <= eval(spider.y + spider.height/2) ){
+                    fly1.active = false;
+                    fly2.active = false;
+                    if(fly1.value == eval("multiples_"+activeMultiple)[NumberSelect]){
+                        sound.play("magic");
+                        TweenMax.to(fly2,0.5,{x:game.width+100});
+                        TweenMax.to(fly1,0.5,{alpha:0});
+                        TweenMax.to(fly1.scale,0.5,{x:1.2,y:1.2,onComplete:newFly1});
+                        spider.setAnimationByName(0, "WIN", true);
+                        star.x = fly1.x;
+                        star.y = fly1.y;
+                        TweenMax.fromTo(star,0.5,{alpha:1},{alpha:0});
+                        TweenMax.fromTo(star.scale,0.5,{x:2,y:2},{x:4,y:4});
+                        
+                    }else{
+                       spider.setAnimationByName(0, "LOSE", true); 
+                        TweenMax.to(fly1,0.5,{x:-100,onComplete:finishGame});
+                        TweenMax.to(fly2,0.5,{x:game.width+100});
+                    }
+
+                    
+                    function newFly1(){
+                        spider.setAnimationByName(0, "IDLE", true);
+                        fly1.alpha = 1;
+                        fly1.y = -200;
+                        fly1.scale.setTo(1,1);
+                        fly1.x = game.width/3 - fly1.width/2;
+                        
+                        fly1.active = true;
+                        
+                        fly2.alpha = 1;
+                        fly2.y = -200;
+                        fly2.scale.setTo(1,1);
+                        fly2.x = fly2.posx;
+                        
+                        fly2.active = true;
+                        
+                        coins++;
+                        xpText.setText(coins);
+                        if(NumberSelect != eval("multiples_" + activeMultiple).length-1){
+                            NumberSelect++;
+                            choiceFly();
+                        }else{
+                            if(counter != 2){
+                                counter = counter+1
+                               activeMultiple = differentsMultiples[counter] 
+                            }else{
+                                counter = 0;
+                                activeMultiple = differentsMultiples[counter];
+                            }
+                            NumberSelect = 0;
+                            choiceFly();
+                        }
+                    }
+                }
+            }
+        });
+        
+        function backSpider(){
+            TweenMax.to(spider,1,{x:spider.posx,ease:Back.easeOut});
+            TweenMax.to(cuerda,1,{x:spider.posx,ease:Back.easeOut});
+        }
+         /*buttonLeft.events.onInputUp.add(function(){
+            TweenMax.to(spider,1,{x:spider.posx,ease:Back.easeOut});
+            TweenMax.to(cuerda,1,{x:spider.posx,ease:Back.easeOut});
+         });*/
+        
+       var buttonRight = new Phaser.Graphics(game);
+        buttonRight.beginFill(0x000000)
+        buttonRight.drawRect(game.world.centerX,0,game.world.width/2, game.height)
+        buttonRight.alpha = 0
+        buttonRight.endFill()
+        buttonRight.inputEnabled = true;
+        sceneGroup.add(buttonRight);
+        buttonRight.events.onInputDown.add(function(){
+            TweenMax.fromTo(spider,0.5,{x:spider.posx},{x:spider.posx + 100,onComplete:backSpider});
+            TweenMax.fromTo(cuerda,0.5,{x:cuerda.posx},{x:cuerda.posx + 100});
+            if(fly2.active){
+                if(fly2.y >= eval(spider.y - spider.height/2) && fly2.y <= eval(spider.y + spider.height/2) ){
+                    fly1.active = false;
+                    fly2.active = false;
+                    if(fly2.value == eval("multiples_"+activeMultiple)[NumberSelect]){
+                        sound.play("magic");
+
+                        TweenMax.to(fly1,0.5,{x:-100});
+                        TweenMax.to(fly2,0.5,{alpha:0});
+                        TweenMax.to(fly2.scale,0.5,{x:1.2,y:1.2,onComplete:newfly2});
+                        spider.setAnimationByName(0, "WIN", true);
+                        star.x = fly2.x;
+                        star.y = fly2.y;
+                        TweenMax.fromTo(star,0.5,{alpha:1},{alpha:0});
+                        TweenMax.fromTo(star.scale,0.5,{x:2,y:2},{x:4,y:4});
+                        
+                    }else{
+                       spider.setAnimationByName(0, "LOSE", true); 
+                        TweenMax.to(fly1,0.5,{x:-100,onComplete:finishGame});
+                        TweenMax.to(fly2,0.5,{x:game.width+100});
+                    }
+
+                    
+                    function newfly2(){
+                        spider.setAnimationByName(0, "IDLE", true);
+                        fly1.alpha = 1;
+                        fly1.y = -200;
+                        fly1.scale.setTo(1,1);
+                        fly1.x = game.width/3 - fly1.width/2;
+                        fly1.active = true;
+                        fly2.alpha = 1;
+                        fly2.y = -200;
+                        fly2.scale.setTo(1,1);
+                        fly2.x = fly2.posx;
+                        fly2.active = true;
+                        
+                        coins++;
+                        xpText.setText(coins);
+                        if(NumberSelect != eval("multiples_" + activeMultiple).length-1){
+                            NumberSelect++;
+                            choiceFly();
+                        }else{
+                            if(counter != 2){
+                                counter = counter+1
+                               activeMultiple = differentsMultiples[counter] 
+                            }else{
+                                counter = 0;
+                                activeMultiple = differentsMultiples[counter];
+                            }
+                            NumberSelect = 0;
+                            choiceFly();
+                        }
+                    }
+                }
+            }            
+        });
+        
+         /*buttonRight.events.onInputUp.add(function(){
+            TweenMax.to(spider,0.5,{x:spider.posx,ease:Back.easeOut});
+            TweenMax.to(cuerda,0.5,{x:spider.posx,ease:Back.easeOut});
+         });*/
+        
+        function choiceFly(){
+            textSpider.setText(activeMultiple);
+        var randomFly = getRandomArbitrary(1, 3);
+            for(i=1;i<=2;i++){
+                eval("fly" + i).value = Math.round(eval("multiples_"+activeMultiple)[NumberSelect]/2);
+                eval("textFly" + i).setText(Math.round(eval("multiples_"+activeMultiple)[NumberSelect]/2));
+            }
+            
+            eval("fly" + randomFly).value = eval("multiples_"+activeMultiple)[NumberSelect];
+            eval("textFly" + randomFly).setText(eval("multiples_"+activeMultiple)[NumberSelect]);
+            
+            console.log("random: " + randomFly);
+            console.log("NumberSelect: " + NumberSelect);
+            console.log("fly1: " + fly1.value);
+            console.log("fly2: " + fly2.value);
+            if(speedGame < 50){
+                speedGame = speedGame + 1;
+            }
+    }
+
+    choiceFly();
+        
+        
+        function finishGame(){
+            TweenMax.to(game,2,{alpha:0,onComplete:gameOver});
+            sound.play("wrong");
+            sound.play("gameLose");
+            bgm.stop();	
+        }	
+	
+		
+		function gameOver(){
+			var resultScreen = sceneloader.getScene("result")
+			resultScreen.setScore(true, coins,gameIndex)
+			sceneloader.show("result");
+		}         
         
         
 		createCoins(coins);
 		createHearts(lives);
-		//createOverlay();
+		createOverlay();
 		
 	}
-
+    
 
 	
 	function update() {
-		
+            if(starGame){
+            if(fly1.y < game.height + fly1.height && fly1.active){
+               fly1.y += speedGame; 
+            }else if(fly1.y > game.height + fly1.height && fly1.active){
+               fly1.y = -200; 
+
+            }
+        
+            if(fly2.y < game.height + fly2.height && fly2.active){
+               fly2.y += speedGame; 
+            }else if(fly2.y > game.height + fly2.height && fly2.active){
+               fly2.y = -200; 
+
+            }
+            }
+            
 	}
 		
 
