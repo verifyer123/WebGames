2557517b758a7f2bd8e09af0ac98d93aa836584a
pzlewinski <pawelranger@hotmail.com>
2017-10-04 20:53:15 -0500

    set transparent to canvas in mathClient for preloaders

--- nonrefactored/mathTournament/mathClient/js/main.js
+++ nonrefactored/mathTournament/mathClient/js/main.js
@@ -1,7 +1,7 @@
 window.minigame = window.minigame || {}
 
 function startGame(){
-	window.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, null, {init: init, create: create }, false, true);
+	window.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.CANVAS, null, {init: init, create: create }, true, true);
     document.body.style.visibility = "hidden"
 
 	function preloadScenes(sceneList){
