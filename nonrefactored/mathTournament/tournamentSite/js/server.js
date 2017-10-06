0bc243f470eb8440f895efc724d616af54102f7d
Aarón Rodríguez <aaron@yogome.com>
2017-10-05 21:13:41 -0500

    MathTournament
    Corrección generateQuestion, loop infinito

--- nonrefactored/mathTournament/tournamentSite/js/server.js
+++ nonrefactored/mathTournament/tournamentSite/js/server.js
@@ -375,15 +375,16 @@ function Server(){
 		// correctAnswer = operand1 +operand2;
 
 		var possibleAnswers = [correctAnswer];
-		var percentage = Math.floor(correctAnswer * 0.25);
+        var percentage = 0;
+        percentage = Math.floor(correctAnswer * 0.25)+1;
 		for(var i = 0; i< NUMBER_OF_FAKE_ANSWERS; i++){
 			var n = correctAnswer;
-			while(n === correctAnswer){
+			while(possibleAnswers.includes(n)){
                 var isSuma = Math.floor((Math.random() * 2) + 1);
                 if(isSuma ==1 )
-					n = correctAnswer + Math.floor(Math.random() * percentage);
+					n = correctAnswer + Math.floor(Math.random() * percentage)+Math.floor(Math.random() * 2);
 				else
-                    n = correctAnswer - Math.floor(Math.random() * percentage);
+                    n = correctAnswer - Math.floor(Math.random() * percentage)-Math.floor(Math.random() * 2);
 			}
 			possibleAnswers.push(n);
 		}
