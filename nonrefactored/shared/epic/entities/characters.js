b3246f9410e4a167813d660bbe5528497d6111b0
pzlewinski <pawelranger@hotmail.com>
2017-12-01 16:58:07 -0600

    epicCharactes update epicBattle

--- nonrefactored/shared/epic/entities/characters.js
+++ nonrefactored/shared/epic/entities/characters.js
@@ -33,8 +33,41 @@ var charactersEntity = function () {
 		return Math.floor(level)
 	}
 
+	function preloadCards(scene, playerCards, enemyCards) {
+		var imageList = scene.assets.images
+		for(var pcardIndex = 0; pcardIndex < playerCards.length; pcardIndex++){
+			var name = playerCards[pcardIndex].id
+			var path = epicCharacters[name].directory
+			path = path.replace(name + ".json", "")
+			var file = path + "/card.png"
+			imageList.push({name:name + "Card", file:file})
+		}
+
+		for(var ecardIndex = 0; ecardIndex < enemyCards.length; ecardIndex++){
+			var name = enemyCards[ecardIndex].id
+			var file = epicCharacters[name].directory + "/card.png"
+			imageList.push({name:name + "Card", file:file})
+		}
+	}
+	
+	function getCard(card) {
+		var cardGroup = game.add.group()
+		var cardBg = cardGroup.create(0,0, card.id + "Card")
+		cardBg.anchor.setTo(0.5, 0.5)
+
+		var fontStyle = {font: "42px VAGRounded", fontWeight: "bold", fill: "#0ca0b7", align: "center"}
+		var level = getLevel(card.xp)
+		var levelText = game.add.text(-78, -100, level, fontStyle)
+		levelText.anchor.setTo(0.5, 0.5)
+		cardGroup.add(levelText)
+
+		return cardGroup
+	}
+
 	return{
 		getLevelXp:getLevelXP,
-		getLevel:getLevel
+		getLevel:getLevel,
+		preloadCards:preloadCards,
+		getCard:getCard
 	}
 }()
