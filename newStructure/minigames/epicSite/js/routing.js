fa60e009c84a6654d9a2d89876e6e57bca92d587
pzlewinski <pawelranger@hotmail.com>
2017-12-15 20:02:14 -0600

    epicModel fix

--- newStructure/minigames/epicSite/js/routing.js
+++ newStructure/minigames/epicSite/js/routing.js
@@ -6,16 +6,6 @@ var routing = function () {
 	var badRouting = false
 	$("#minigames").show()
 
-	function getParameterByName(name, url) {
-		if (!url) url = window.location.href;
-		name = name.replace(/[\[\]]/g, "\\$&");
-		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-			results = regex.exec(url);
-		if (!results) return null;
-		if (!results[2]) return '';
-		return decodeURIComponent(results[2].replace(/\+/g, " "));
-	}
-
 	// router.on(
 	// 	function () {
 	// 		$("#minigames").hide()
