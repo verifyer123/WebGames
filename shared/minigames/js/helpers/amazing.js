e175cdce8e11a4b9452d022a483b76b514d6adaf
David <david@yogome.com>
2018-02-01 18:58:15 -0600

    amazing, rafaga de tamales banners

--- shared/minigames/js/helpers/amazing.js
+++ shared/minigames/js/helpers/amazing.js
@@ -80,7 +80,7 @@ amazing.getGames = function(){
 		{name:'Net Shoes',iconName:'net',url:'http://amazingapp.mx/juegos/netshoes/',coupon : false,mixName:'netshoes',id:5634101323235328},//16
         {name:'Coffee Rush',iconName:'coffeerush',url:'http://amazingapp.mx/juegos/coffeerush/',coupon : false,mixName:'coffeerush',id:5662438108168192},//17
         {name:'2+2',iconName:'2+2',url:'http://amazingapp.mx/juegos/game2Plus2/',coupon : false,mixName:'2+2',id:6293705958883328},//18
-        {name:'Rafaga de Tamales',iconName:'rafagaDeTamales',url:'http://amazingapp.mx/juegos/rafagaDeTamales/',coupon : false,mixName:'rafagaDeTamales',id:5679382827892736},//19
+        {name:'Rafaga de\n Tamales',iconName:'rafagaDeTamales',url:'http://amazingapp.mx/juegos/rafagaDeTamales/',coupon : false,mixName:'rafagaDeTamales',id:5679382827892736},//19
         {name:'Ski Zag',iconName:'skiZag',url:'http://amazingapp.mx/juegos/skiZag/',coupon : false,mixName:'skiZag',id:5766289444306944},//20
         //{name:'Vips',iconName:'vips',url:'http://amazingapp.mx/juegos/vips/',coupon : false,mixName:'vips',id:5303856053354496},
     ]
