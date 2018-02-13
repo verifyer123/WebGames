41647b659c5ee8847cbd36406350989631211701
pzlewinski <pawelranger@hotmail.com>
2018-02-13 16:11:03 -0600

    orientationChange instead of resize in epicSite

--- newStructure/minigames/epicMap/js/scenes/map.js
+++ newStructure/minigames/epicMap/js/scenes/map.js
@@ -1414,7 +1414,7 @@ var map = function(){
 		assets: assets,
 		name: "map",
 		update: update,
-		preload:preload,getGameData:function () { var games = yogomeGames.getGames(); return games[gameIndex];},
+		preload:preload,
 		create: function(event){
 
 			initialize()
