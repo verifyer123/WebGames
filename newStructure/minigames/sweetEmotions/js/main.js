24fda93838ff8679a05f3a36b9a46ea4df3b7b23
pzlewinski <pawelranger@hotmail.com>
2018-02-14 12:48:52 -0600

    detail fix in pin (size buttons), library change to fix out focus iframe phaser game

--- newStructure/minigames/sweetEmotions/js/main.js
+++ newStructure/minigames/sweetEmotions/js/main.js
@@ -39,9 +39,10 @@ function startGame(){
 
         game.stage.backgroundColor = "#ffffff"
         game.time.advancedTiming = true
-        game.stage.disableVisibilityChange = true;        
+        game.stage.disableVisibilityChange = true;
 
-        game.plugins.add(Fabrique.Plugins.Spine);
+        // game.plugins.add(Fabrique.Plugins.Spine);
+		game.plugins.add(PhaserSpine.SpinePlugin);
         
         var language = "EN"
         if(window.location.search){
