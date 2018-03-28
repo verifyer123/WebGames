f20986a337cc21aeacecf327687c38279033c611
geovanniYogome <geovanni@yogome.com>
2018-03-27 19:06:20 -0600

    Se inicio el proyecto Quantrix

---
+++ newStructure/quantrix/dist/js/main.js
@@ -0,0 +1,15 @@
+var _QUANTRIX = {
+  VERSION : "0.1.0",
+  SERVICES : {
+    LOGIN : "",
+    GAME_FINISHED : ""
+  },
+  DEFAULT_GAMES_ROUTE : "minigames/",
+  DEFAULT_GAME : "additiondojo",
+  GAMES : {
+    "additiondojo" : "additiondojo",
+    "acornNumbers" : "acornNumbers",
+    "animalRoute" : "animalRoute",
+    "aracnumber" : "aracnumber"
+  }
+}
