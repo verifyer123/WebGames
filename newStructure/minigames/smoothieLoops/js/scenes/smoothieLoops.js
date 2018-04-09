4d6af3417bf4062cddca1b5841b1bcb4a3b7150e
David <david@yogome.com>
2018-04-09 14:55:45 -0500

    smoothieLoops fix

--- newStructure/minigames/smoothieLoops/js/scenes/smoothieLoops.js
+++ newStructure/minigames/smoothieLoops/js/scenes/smoothieLoops.js
@@ -63,7 +63,7 @@ var smoothieLoops = function(){
     }
 
     var NUM_LIFES = 3
-    var LEVEL_TIMER = 0
+    var LEVEL_TIMER = 2
     var INITIAL_TIME = 15000
     var DELTA_TIME = 100
     var MIN_TIME = 10000
