f1767d73249f731eeec0b7de356edf031471e129
Rodrigo Escobar Gutierrez <rockdrizt@gmail.com>
2018-06-07 12:15:41 -0500

    Mambo Jump O beta

--- newStructure/shared/minigames/js/helpers/yogomeGames.js
+++ newStructure/shared/minigames/js/helpers/yogomeGames.js
@@ -248,6 +248,7 @@ var yogomeGames = function () {
 			{name:'Measuridge',url:urlSet + 'measuridge/',mapUrl:urlMap + 'measuridge', sceneName:'measuridge',subject:'math', review:false,objective:15,demo:false, type:gameTypeEnum.COUNT},//217
 			{name:'H2os',url:urlSet + 'h2os/',mapUrl:urlMap + 'h2os', sceneName:'h2os',subject:'math',review:false,objective:30,demo:false,type:gameTypeEnum.COUNT},//218
 			{name:'UpRoar',url:urlSet + 'upRoar/',mapUrl:urlMap + 'upRoar', sceneName:'upRoar',subject:'language',review:false,objective:20,demo:false,type:gameTypeEnum.CHOOSE},//219
+			{name:'Mambo Jump O',url:urlSet + 'mamboJumpO/',mapUrl:urlMap + 'mamboJumpO', sceneName:'mamboJumpO',subject:'creativity',review:false,objective:25,demo:false,type:gameTypeEnum.MATCH},//220
 		]
 
 		for(var gIndex = 0; gIndex < games.length; gIndex++){
