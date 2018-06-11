020b590c6502984ae73dcd982cb9ce2e1c19ce2a
geovanniYogome <geovanni@yogome.com>
2018-06-11 14:49:48 -0500

    Se agregaron archivos de template a las estucturas base

---
+++ newStructure/estructurasBase/templateFiles/site/js/main.js
@@ -0,0 +1,23 @@
+var APP = {
+    /**
+    * Versión del proyecto.
+    */
+    VERSION : "0.1.0",
+    LANGUAGE_QUERY : "?language=es",
+    SUBJECT : "tech",
+    /**
+    * Nombre de las variables en el localstorage
+    */
+    LOCALSTORAGE : {
+        TOKEN : "child_token",
+        CHILD_ID : "child_id",
+        YOGOTAR  : "yogotar_id",
+    },
+    /**
+    * Links absolutos de los endpoints.
+    */
+    SERVICES : {
+        // Por ejemplo:
+        // LOGIN : "https://math-tournament2-dot-heroesofknowledge.appspot.com/tournament/child/v1/login"
+    },
+}
