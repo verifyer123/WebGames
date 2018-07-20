aa45542cec5edfa7982053be7f71102065af7f3b
DiegoFRM <diego@yogome.com>
2018-07-20 12:51:00 -0500

    update list

--- listadeJuegos/src/js/main.js
+++ listadeJuegos/src/js/main.js
@@ -8,25 +8,49 @@ $(document).on('ready', function() {
     
     for(var i = 0; i<= optimized.length-1;i++){
         $("#optimizedGames").append(
-    `<div id="btn`+i+`">
-      <img src="../../newStructure/minigames/`+ optimized[i].mapUrl +`/images/fbpost.png">
+    `<div id="btnOptimized`+i+`">
+      <img src="../../newStructure/minigames/`+ optimized[i].url +`/images/fbpost.png">
     </div>
     `);
+        $("#btnOptimized" + i).attr("number",i);
+        $("#btnOptimized" + i).css("opacity",0.5);
+        $("#btnOptimized" + i).mouseover(function(){
+            $(this).css("opacity",1);
+        });   
+        $("#btnOptimized" + i).mouseout(function(){
+            $(this).css("opacity",0.5);
+        });   
+        $("#btnOptimized" + i).click(function(){
+            var place = $(this).attr("number")
+           window.location = "../../newStructure/gamesite/#/minigames/Optimized" + optimized[place].url
+        });   
     }   
     
     for(var i = 0; i<= onGoing.length-1;i++){
         $("#onGoingGames").append(
-    `<div id="btn`+i+`">
+    `<div id="btnGoing`+i+`">
       <img src="../../newStructure/minigames/`+ onGoing[i].url +`images/fbpost.png">
     </div>
     `);
+        $("#btnGoing" + i).attr("number",i);
+        $("#btnGoing" + i).css("opacity",0.5);
+        $("#btnGoing" + i).mouseover(function(){
+            $(this).css("opacity",1);
+        });
+        $("#btnGoing" + i).mouseout(function(){
+            $(this).css("opacity",0.5);
+        });
+           
+        $("#btnGoing" + i).click(function(){
+            var place = $(this).attr("number")
+           window.location = "../../newStructure/gamesite/#/minigames/" + onGoing[place].url
+        });   
     }    
     
     $("#totalOptimized").text(" (" + optimized.length +")")
     $("#totalOptimizedLess").text(" (" +  onGoing.length +")")
     
-    $("#optimizedGames").find(".slick-next").click(function(){
-    });
+
 
 
     $("#optimizedGames").slick({
