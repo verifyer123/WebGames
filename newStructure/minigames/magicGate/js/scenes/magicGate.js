52c47ba0ddddf39f89c6f546acbc16f871e2d062
heber <hebersadurni@yogome.com>
2018-08-01 10:04:02 -0500

    arc posY

--- newStructure/minigames/magicGate/js/scenes/magicGate.js
+++ newStructure/minigames/magicGate/js/scenes/magicGate.js
@@ -386,7 +386,7 @@ var magicGate = function(){
 		}
 		
 		for(var placeArcs=0; placeArcs<game.world.width;placeArcs+=133){
-			var arcs = sceneGroup.create(placeArcs,game.world.centerY,'arc');
+			var arcs = sceneGroup.create(placeArcs,game.world.centerY+100,'arc');
 			arcs.anchor.setTo(0.5,0.5)
 		}
 
