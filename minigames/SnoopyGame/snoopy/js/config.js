e8dee0d995463173268d9a8b51426d7404bab4b1
David <davidsanchezgarduo@gmail.com>
2018-09-03 10:15:31 -0500

    cleaning unused files

--- minigames/SnoopyGame/snoopy/js/config.js
+++
@@ -1,8 +0,0 @@
-window.amazing = {
-	config: {
-		name: "Snoopy en Busca del Sabor",
-		language: "en",
-		minigameUrl: "../snoopyEnBuscaDelSabor/index.html",
-		desktopUrl: "./desktop.html",
-	}
-}
