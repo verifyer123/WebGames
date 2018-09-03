a765f674007e64083dbc122cd27a5185dd2648d6
hebersadurni <arsehase@live.com.mx>
2018-09-03 10:37:41 -0500

    tutorialImagic bug ES fixed

--- newStructure/shared/minigames/js/helpers/tutorialImagic.js
+++ newStructure/shared/minigames/js/helpers/tutorialImagic.js
@@ -222,8 +222,8 @@ var tutorialHelper = function () {
 		currentLoader.image(backKeyImagic,sharePath+"images/tutorial/background_tutorial_Imagic.png");
 		currentLoader.image(backKeyStars,sharePath+"images/tutorial/back_stars.png");
 		if(language == "ES"){
-			currentLoader.image(howkeyWeb,sharePath+'images/tutorial/how_ES_Imagic.png')
-			currentLoader.image(playKeyWeb,sharePath+'images/tutorial/play_Imagic.png')
+			currentLoader.image(howkeyImagic,sharePath+'images/tutorial/how_ES_Imagic.png')
+			currentLoader.image(playKeyImagic,sharePath+'images/tutorial/play_Imagic.png')
 		}
 		else{
 			currentLoader.image(howkeyImagic,sharePath+'images/tutorial/how_EN_Imagic.png')
