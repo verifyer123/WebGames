8d8439653ec2dbb751d0c3dd5b2c2b74c0794200
Dafne Bernardino <dafne.bernardino@yogome.com>
2018-09-27 09:10:51 -0500

    Changing list u_u try again

--- newStructure/shared/minigames/js/helpers/optimizedGames.js
+++ newStructure/shared/minigames/js/helpers/optimizedGames.js
@@ -52,6 +52,7 @@ var optimizedGames={
 		{name:'Optimized Beat O Matic',url:'beatOMatic/',mapUrl:'beatOMatic', sceneName:'beatOMatic',subject:'creativity', review:false,objective:10,demo:false, type:gameTypeEnum.SEQUENCE},
 		{name:'Optimized Feather Shelter',url:'featherShelter/',mapUrl:'featherShelter', sceneName:'feather',subject:'math',review:false,objective:10,demo:false,type:gameTypeEnum.COUNT},
 		{name:'Optimized Pizza Fraction',url:'pizzafraction/',mapUrl:'pizzafraction', sceneName:'pizzafraction',subject:'math',review:true,objective:25,demo:false,type:gameTypeEnum.MATCH},
+		
 	]
 	
 }
